33c23e4442587d69563c62c3bd83437bbbd2ed9f
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2024-03-01 13:51:10 +0530

    Add files via upload

---
+++ FirstAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WebAPI_Tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get()
+        {
+            return Ok("Your first API call is success");
+        }     
+
+        [HttpPost]
+        public IActionResult Post(JObject data)
+        {
+            data["value"] = "arul";
+           return Ok(data);
+            // If get this point we can connect to database and send it back with some data
+        }
+    }
+}
